a70f31dd5d505d4347893775edff59593647da33
Peks <0peks0@proton.me>
2024-03-24 13:33:37 -0600

    rotate arrow icon option added

--- src/components/svg-wrapper.jsx
+++ src/components/svg-wrapper.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-const SvgWrapper = ({ viewBox, children }) => {
+const SvgWrapper = ({ viewBox, children, rotate }) => {
   return (
-    <svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox={viewBox}>
+    <svg
+      version="1.0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={viewBox}
+      style={{ transform: rotate && "rotate(180deg)" }}
+    >
       {children}
     </svg>
   );
