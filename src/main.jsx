abb53215eed0b8566350ff4481878d2dec38cf76
Peks <0peks0@proton.me>
2024-03-24 12:04:51 -0600

    clean actions added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
-import UserProvider from "./context/user/userProvider.jsx";
+import UserProvider from "./context/user/userProvider";
+import { useStepFormStore } from "@/context/stepFormStore";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </React.StrictMode>
-);
+const Index = () => {
+  const { resetStepForm } = useStepFormStore();
+  useEffect(() => {
+    // Acciones de limpieza al cargar la aplicación
+
+    return () => {
+      // Acciones de limpieza al desmontar la aplicación (cuando se cierre la pestaña del navegador, por ejemplo)
+      resetStepForm();
+    };
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Index />);
