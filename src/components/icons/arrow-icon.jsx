faa455e7a9274f698ca506e2b20c1fddf8bcb258
Peks <0peks0@proton.me>
2024-03-24 10:20:55 -0600

    first icons components created

---
+++ src/components/icons/arrow-icon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import SvgWrapper from "@/components/svg-wrapper";
+
+const ArrowIcon = () => {
+  return (
+    <SvgWrapper viewBox={"0 0 25 25"}>
+      <g clipPath="url(#a)">
+        <path d="M9.08 16.763v3.21a.909.909 0 0 1-.637.87c-.391.122-.757.052-1.099-.21-2.754-2.113-5.101-4.417-7.04-6.91-.429-.55-.404-1.111.074-1.684 2.246-2.686 4.599-4.963 7.057-6.83a1.024 1.024 0 0 1 1.645.814v3.278c0 .19.095.292.285.308l14.075 1.024c1.066.08 1.586.655 1.559 1.725l-.034 1.309c-.023 1.054-.56 1.62-1.61 1.696L9.438 16.376c-.24.019-.359.148-.359.387Z" />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h25v25H0z" />
+        </clipPath>
+      </defs>
+    </SvgWrapper>
+  );
+};
+
+export default ArrowIcon;
---
+++ src/components/icons/close-icon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SvgWrapper from "@/components/svg-wrapper";
+
+const CloseIcon = () => {
+  return (
+    <SvgWrapper viewBox="0 0 30 31">
+      <path d="M5.625 4.25L3.75 6.125L11.5622 15.5L3.75 24.875L5.625 26.75L15 18.9378L24.375 26.75L26.25 24.875L18.4378 15.5L26.25 6.125L24.375 4.25L15 12.0622L5.625 4.25Z" />
+    </SvgWrapper>
+  );
+};
+
+export default CloseIcon;
